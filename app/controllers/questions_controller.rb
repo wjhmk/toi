e6b4b231a9dd49f3affb6c5540940f668af43cc5
ren <32934023+wjhmk@users.noreply.github.com>
2019-01-30 01:11:11 +0900

    questionコントローラー、モデル作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,24 @@
 class QuestionsController < ApplicationController
+
+    def index
+      @questions = Question.all
+    end
+
+    def new
+      @question = Question.new
+    end
+
+    def create
+      @question = Question.new(question_params)
+
+        if @question.save
+            redirect_to @question
+        end
+
+    end
+
+    private
+          def question_params
+            params.require(:question).permit(:question_image, :answer, :column, :hint, )
+          end
 end
