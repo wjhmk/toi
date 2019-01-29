bd1fdf1dd8cd8f6f0b7494ac0ac2f2151fd74313
ren <32934023+wjhmk@users.noreply.github.com>
2019-01-30 01:25:05 +0900

    基本的なフォーム作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,11 +10,13 @@ class QuestionsController < ApplicationController
 
     def create
       @question = Question.new(question_params)
-
         if @question.save
             redirect_to @question
         end
+    end
 
+    def show
+      @question = Question.find(params[:id])
     end
 
     private
